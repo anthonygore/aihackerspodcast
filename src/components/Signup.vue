ed1c855c241cdc33577f0320947ec3411161bca8
Anthony Gore <hi@anthonygore.com>
2023-03-27 13:37:48 +1100

    Adding success message

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,6 +2,8 @@
 import {ref} from "vue";
 
 const email = ref('')
+const isSuccess = ref(false)
+
 async function submit() {
   if (email.value.length) {
     const res = await fetch('/.netlify/functions/signup', {
@@ -11,8 +13,12 @@ async function submit() {
         "Content-Type": "application/json",
       },
     })
-    const data = await res.json()
-    console.log(data)
+    if (res.status === 200) {
+      isSuccess.value = true
+    } else {
+      const data = await res.json()
+      console.log(data)
+    }
   }
 }
 </script>
@@ -23,7 +29,10 @@ async function submit() {
       <div class="text-center mx-auto max-w-2xl text-center flex flex-col" id="subscribe">
         <h2 class="text-4xl font-bold text-white pb-6">Get notified when we’re launching</h2>
         <p class="text-lg text-indigo-200 pb-10"><strong>AI Hackers Podcast</strong> will be launching soon. Please leave you're email to be notified of the first episode. No spam, one-click unsubscribe.</p>
-        <div class="mx-auto flex gap-x-2">
+        <div v-if="isSuccess" class="bg-green-500 rounded-md p-4 text-green-50">
+          You're subscribed!
+        </div>
+        <div class="mx-auto flex gap-x-2" v-else>
           <label for="email-address" class="sr-only">Email address</label>
           <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required class="w-72 flex-auto rounded-md border-0 bg-white px-3.5 py-2" placeholder="Enter your email">
           <button type="button" @click="submit" class="flex-none rounded-md bg-white py-2.5 px-3.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Notify me</button>
