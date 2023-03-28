3a00401502e12db1a4361fa876b20b25b2fd0480
Anthony Gore <hi@anthonygore.com>
2023-03-28 12:56:48 +1100

    Copy fixes

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,7 +8,7 @@
       <div class="prose prose-lg max-w-none">
         <h2 class="font-serif">About</h2>
         <p>In 2023, AI tools like ChatGPT and Midjourney are allowing developers to perform tasks that were inconceivable only months ago.</p>
-        <p>This explosion of powerful new technology has created unprecedented opportunity for developers and entrepreneurs to provide new products and solutions.</p>
+        <p>This explosion of powerful new technology has created an unprecedented opportunity for developers and entrepreneurs to provide new products and solutions.</p>
         <p>Despite this opportunity, there are many challenges. For example, how can AI products be unique and find a durable competitive advantage?</p>
         <p>In this podcast, we’ll chat with AI hackers to hear what they’re building and learn about the tools and techniques that are empowering them to create.</p>
       </div>
